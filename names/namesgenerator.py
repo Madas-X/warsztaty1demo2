82f08dd8279238f6f343ea5b71d451f9564cae41
t.szczygielski <t.szczygielski@polsource.com>
2019-03-05 15:04:00 +0100

    getting rid of random

--- names/namesgenerator.py
+++ names/namesgenerator.py
@@ -1,11 +1,11 @@
-from Lib import random
+# from Lib import random
 from names import adjectives
 from names import surnames
 
 
 def get_random_name():
-    name = surnames.values[random.randrange(len(surnames.values))] + "_" + adjectives.values[
-        random.randrange(len(adjectives.values))]
-    if name == "boring_wozniak":
-        name = get_random_name()
-    return name
+    # name = surnames.values[random.randrange(len(surnames.values))] + "_" + adjectives.values[
+        # random.randrange(len(adjectives.values))]
+    # if name == "boring_wozniak":
+    #     name = get_random_name()
+    return "boring_wozniak"
